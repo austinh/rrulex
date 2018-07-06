6083a6ec3aadf1b96e69b76e5d3520f1b08126df
Austin Hammer <austinlhammer@gmail.com>
2018-07-06 19:32:01 -0400

    adding precommit

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule Rrulex.MixProject do
       {:timex, "~> 3.3"},
       {:ex_doc, "~> 0.18", only: :dev},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
+      {:pre_commit, "~> 0.3.4", only: :dev}
     ]
   end
 
